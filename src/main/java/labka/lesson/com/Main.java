aaf62472228437af86184c943088d6b0ac2930b7
Varcixis4 <gentley.comp@gmail.com>
2022-08-30 01:44:57 +0300

    first labka_02

--- src/main/java/labka/lesson/com/Main.java
+++ src/main/java/labka/lesson/com/Main.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-
+//change
 public class Main
 {
     static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
